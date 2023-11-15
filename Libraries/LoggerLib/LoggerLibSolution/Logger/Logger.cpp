66a21ff0962e08250c010f1bae6b245a9ed64156
COGMLS <mlsmatheus@hotmail.com>
2023-11-15 16:59:08 -0300

    Added LoggerLib from SmartAutoRun. This library was created to be used for generic purposes.

---
+++ Libraries/LoggerLib/LoggerLibSolution/Logger/Logger.cpp
@@ -0,0 +1,367 @@
+#include "pch.h"
+#include "Logger.hpp"
+
+Logger::Logger(std::filesystem::path logPath, std::string baseFileName)
+{
+	this->autoSaveLogEntries = false;
+	this->maxLogEntries = 0;
+	this->lastLogPos = 0;
+
+	this->setLogStatus(LoggerError::LOGGER_ERROR_OK);
+
+	if (std::filesystem::exists(logPath))
+	{
+		this->logPath = logPath;
+	}
+	else
+	{
+		if (std::filesystem::create_directory(logPath))
+		{
+			this->logPath = logPath;
+		}
+		else
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_PATH_DONT_EXIST);
+		}
+	}
+
+	bool baseFileNameOk = true;
+
+	// Search for invalid chars for file's name
+	for (size_t i = 0; i < baseFileName.size(); i++)
+	{
+		if (baseFileNameOk)
+		{
+			switch (baseFileName[i])
+			{
+				case '\\':
+				case '/':
+				case ':':
+				case '*':
+				case '?':
+				case '"':
+				case '<':
+				case '>':
+					this->setLogStatus(LoggerError::LOGGER_ERROR_BASE_FILE_NAME_HAS_UNUSPPORTED_CHAR);
+					baseFileNameOk = false;
+					break;
+				default:
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	if (this->lastLogStatus == LoggerError::LOGGER_ERROR_OK && this->logStatus.size() == 0)
+	{
+		this->dtCreation = getLoggerDateTime();
+
+		this->baseFileName = baseFileName + "_" + convertDateTime2LogStrEntry(dtCreation, true);
+
+		this->logFilePath = std::filesystem::path(this->logPath.string() + LOGGER_DIRECTORY_PATH_SEPARATOR + this->baseFileName + LOGGER_LOG_FILE_TYPE);
+
+		// Create the file and make it ready to be used for write:
+
+		std::fstream logFile;
+		logFile.open(logFilePath.string(), std::ios::out);
+
+		//this->logFile.open(logFilePath.string(), std::ios::out);
+
+		if (!logFile.is_open())
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_CAN_NOT_CREATE_FILE);
+		}
+		else
+		{
+			logFile.close();
+		}
+
+		if (!std::filesystem::exists(this->logFilePath))
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_FILE_CAN_NOT_BE_FOUNDED);
+		}
+	}
+}
+
+Logger::Logger(const Logger& other)
+{
+	this->baseFileName = other.baseFileName;
+	this->lastLogStatus = other.lastLogStatus;
+	this->logEntries = other.logEntries;
+	//this->logFile = other.logFile;
+	this->logFilePath = other.logFilePath;
+	this->logPath = other.logPath;
+	this->logStatus = other.logStatus;
+	this->dtCreation = other.dtCreation;
+	this->autoSaveLogEntries = other.autoSaveLogEntries;
+	this->maxLogEntries = other.maxLogEntries;
+	this->lastLogPos = other.lastLogPos;
+}
+
+Logger::Logger(Logger&& other) noexcept
+{
+	this->baseFileName = other.baseFileName;
+	this->lastLogStatus = other.lastLogStatus;
+	this->logEntries = other.logEntries;
+	//this->logFile = other.logFile;
+	this->logFilePath = other.logFilePath;
+	this->logPath = other.logPath;
+	this->logStatus = other.logStatus;
+	this->dtCreation = other.dtCreation;
+	this->autoSaveLogEntries = other.autoSaveLogEntries;
+	this->maxLogEntries = other.maxLogEntries;
+	this->lastLogPos = other.lastLogPos;
+}
+
+Logger& Logger::operator=(const Logger& other)
+{
+	this->baseFileName = other.baseFileName;
+	this->lastLogStatus = other.lastLogStatus;
+	this->logEntries = other.logEntries;
+	//this->logFile = other.logFile;
+	this->logFilePath = other.logFilePath;
+	this->logPath = other.logPath;
+	this->logStatus = other.logStatus;
+	this->dtCreation = other.dtCreation;
+	this->autoSaveLogEntries = other.autoSaveLogEntries;
+	this->maxLogEntries = other.maxLogEntries;
+	this->lastLogPos = other.lastLogPos;
+
+	return *this;
+}
+
+Logger& Logger::operator=(Logger&& other) noexcept
+{
+	this->baseFileName = other.baseFileName;
+	this->lastLogStatus = other.lastLogStatus;
+	this->logEntries = other.logEntries;
+	//this->logFile = other.logFile;
+	this->logFilePath = other.logFilePath;
+	this->logPath = other.logPath;
+	this->logStatus = other.logStatus;
+	this->dtCreation = other.dtCreation;
+	this->autoSaveLogEntries = other.autoSaveLogEntries;
+	this->maxLogEntries = other.maxLogEntries;
+	this->lastLogPos = other.lastLogPos;
+
+	other.~Logger();
+
+	return *this;
+}
+
+bool Logger::operator==(const Logger& other) const
+{
+	return this->baseFileName == other.baseFileName &&
+		this->lastLogStatus == other.lastLogStatus &&
+		this->logEntries == other.logEntries &&
+		this->logFilePath == other.logFilePath &&
+		this->logPath == other.logPath &&
+		this->logStatus == other.logStatus &&
+		this->dtCreation.calendar == other.dtCreation.calendar &&
+		this->dtCreation.hours == other.dtCreation.hours &&
+		this->dtCreation.minutes == other.dtCreation.minutes &&
+		this->dtCreation.mSeconds == other.dtCreation.mSeconds &&
+		this->dtCreation.seconds == other.dtCreation.seconds &&
+		this->dtCreation.weekday == other.dtCreation.weekday &&
+		this->autoSaveLogEntries == other.autoSaveLogEntries &&
+		this->maxLogEntries == other.maxLogEntries &&
+		this->lastLogPos == other.lastLogPos;
+}
+
+int Logger::saveLog()
+{
+	if (this->autoSaveLogEntries)
+	{
+		if (this->writeLogFile(true))
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_SAVE_LOG_ENTRIES_OK);
+			return 0;
+		}
+		else
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_SAVE_LOG_ENTRIES_FAIL);
+			return 1;
+		}
+	}
+	else
+	{
+		if (this->writeLogFile(false))
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_SAVE_LOG_ENTRIES_OK);
+			return 0;
+		}
+		else
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_SAVE_LOG_ENTRIES_FAIL);
+			return 1;
+		}
+	}
+}
+
+Logger::~Logger()
+{
+
+}
+
+std::filesystem::path Logger::getLogDirectoryPath()
+{
+	return this->logPath;
+}
+
+std::filesystem::path Logger::getLogFilePath()
+{
+	return this->logFilePath;
+}
+
+LoggerLocalDateTime Logger::getLogDateTime()
+{
+	return this->dtCreation;
+}
+
+bool Logger::writeLogFile(bool ateMode)
+{
+	try
+	{
+		bool isLogFileCreated = true;
+
+		for (LoggerError& errCode : this->logStatus)
+		{
+			if (errCode == LoggerError::LOGGER_ERROR_CAN_NOT_CREATE_FILE || errCode == LoggerError::LOGGER_ERROR_FILE_CAN_NOT_BE_FOUNDED)
+			{
+				isLogFileCreated = false;
+			}
+		}
+
+		if (isLogFileCreated)
+		{
+			std::fstream logFile;
+
+			if (ateMode)
+			{
+				logFile.open(this->logFilePath.string(), std::ios::out | std::ios::in);
+			}
+			else
+			{
+				logFile.open(this->logFilePath.string(), std::ios::out);
+			}
+
+			// Set the last position to write the log, if the maxLogEntries and autoSaveLogEntries are defined
+			if (this->maxLogEntries > 0 && this->autoSaveLogEntries)
+			{
+				int g = 0;
+
+				std::string line;
+				while (std::getline(logFile, line))
+				{
+					#pragma warning (suppress : 4244)
+					g = logFile.tellg();
+				}
+
+				if (g >= 0)
+				{
+					this->lastLogPos = g;
+				}
+
+				// Set to clear the flags and define a new position for writing and reading
+				if (logFile.tellg() != 0 || logFile.tellg() != 0)
+				{
+					logFile.clear();
+					logFile.seekg(this->lastLogPos);
+					logFile.seekp(this->lastLogPos);
+				}
+			}
+
+			// If the log file was created with success, save the entries
+			if (isLogFileCreated)
+			{
+				for (LogEntry& entry : this->logEntries)
+				{
+					logFile << entry.getEntry() << std::endl;
+				}
+			}
+
+			if (isLogFileCreated)
+			{
+				if (logFile.is_open())
+				{
+					logFile.close();
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+	catch (const std::exception& e)
+	{
+		this->logEntries.push_back(LogEntry("[EXCEPTION]::", e.what()));
+		return false;
+	}
+}
+
+void Logger::setLogStatus(LoggerError errCode)
+{
+	this->lastLogStatus = errCode;
+
+	if (errCode != LoggerError::LOGGER_ERROR_OK)
+	{
+		this->logStatus.push_back(errCode);
+	}
+}
+
+LoggerError Logger::autoSave()
+{
+	if (this->maxLogEntries > 0)
+	{
+		if (this->logEntries.size() >= this->maxLogEntries)
+		{
+			this->setLogStatus(LoggerError::LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_FULL);
+
+			bool logStatus = this->writeLogFile(true);
+
+			this->logEntries.clear();
+
+			if (logStatus)
+			{
+				return LoggerError::LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_OK;
+			}
+			else
+			{
+				return LoggerError::LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_FAIL;
+			}
+		}
+
+		return LoggerError::LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_NOT_FULL;
+	}
+	else
+	{
+		if (this->saveLog() == 0)
+		{
+			return LoggerError::LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_OK;
+		}
+		
+		return LoggerError::LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_FAIL;
+	}
+}
+
+void Logger::newEntry(LogEntry entry)
+{
+	if (this->autoSaveLogEntries)
+	{
+		this->setLogStatus(this->autoSave());
+	}
+
+	this->logEntries.push_back(entry);
+}
+
+void Logger::setAutoSave(bool autoSave, size_t maxLogEntries)
+{
+	this->autoSaveLogEntries = autoSave;
+	this->maxLogEntries = maxLogEntries;
+}
---
+++ Libraries/LoggerLib/LoggerLibSolution/Logger/Logger.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+#include "pch.h"
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <filesystem>
+#include <chrono>
+
+#include "LoggerCommonDataTypes.hpp"
+#include "LoggerDateTime.hpp"
+
+enum LOGGER_LIB_API LoggerError : unsigned int
+{
+	LOGGER_ERROR_OK,
+	LOGGER_ERROR_PATH_DONT_EXIST,
+	LOGGER_ERROR_BASE_FILE_NAME_HAS_UNUSPPORTED_CHAR,
+	LOGGER_ERROR_CAN_NOT_CREATE_FILE,
+	LOGGER_ERROR_FILE_CAN_NOT_BE_FOUNDED,
+	LOGGER_ERROR_SAVE_LOG_ENTRIES_OK,
+	LOGGER_ERROR_SAVE_LOG_ENTRIES_FAIL,
+	LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_OK,
+	LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_FAIL,
+	LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_FULL,
+	LOGGER_ERROR_AUTOSAVE_LOG_ENTRIES_NOT_FULL
+};
+
+class LOGGER_LIB_API Logger
+{
+	private:
+		std::filesystem::path logPath;			// Log folder
+		std::string baseFileName;				// Log file base name
+		std::filesystem::path logFilePath;		// Log file path
+		std::vector<LogEntry> logEntries;		// Log entries
+		unsigned int lastLogStatus;				// Last log error code registered
+		std::vector<LoggerError>logStatus;		// History of all error codes inside the Logger object
+		LoggerLocalDateTime dtCreation;			// Log date and time creation
+		size_t maxLogEntries;					// Define a maximum size for logEntries
+		bool autoSaveLogEntries;				// Define if true and the maxLogEntries is bigger than 0, the auto save will automatically save the entries to the file
+		int lastLogPos;							// Last log position, used for locate the last position when using maxLogEntries and autoSaveLogEntries
+
+		/// @brief Set the logger error code status. This also add to the error list, except when is LOGGER_ERROR_OK.
+		/// @param errCode Logger error code 
+		void setLogStatus(LoggerError errCode);
+
+		/// @brief Write the log entries on the disk
+		bool writeLogFile(bool ateMode);
+
+		/// @brief Save automatically the entries and clean the stored entries in memory
+		LoggerError autoSave();
+
+	public:
+		/// @brief Create a logger register
+		/// @param logPath Path where the log will be saved
+		/// @param baseFileName Base name for create the log file, the rest of name will be used the date and time of creation
+		Logger(std::filesystem::path logPath, std::string baseFileName);
+
+		/// @brief Copy a logger to another
+		/// @param other Other logger data reference
+		Logger(const Logger& other);
+
+		/// @brief Move a logger to another
+		/// @param other Other logger data that will receive the reference data
+		Logger(Logger&& other) noexcept;
+
+		/// @brief Copy assignment
+		/// @param other Other logger reference data
+		Logger& operator=(const Logger& other);
+
+		/// @brief Move assignment
+		/// @param other Other logger reference data
+		Logger& operator=(Logger&& other) noexcept;
+
+		/// @brief Equality operator
+		/// @param other Other logger reference data
+		/// @return True if is the same memory reference for all data, otherwise will be a False value
+		bool operator==(const Logger& other) const;
+
+		/// @brief Destroy the logger object and save the entries
+		~Logger();
+
+		/// @brief Get the log folder path
+		std::filesystem::path getLogDirectoryPath();
+
+		/// @brief Get the actual log file path 
+		std::filesystem::path getLogFilePath();
+
+		/// @brief Get the log date and time creation
+		LoggerLocalDateTime getLogDateTime();
+
+		/// @brief Add a new entry for the logger
+		void newEntry(LogEntry entry);
+
+		/// @brief Save the log to a file
+		int saveLog();
+
+		/// @brief Define the autosave enabled or disabled and define if the logger will hold a limit of entries on the memory before save then on files
+		/// @param autoSave Define if the auto save will be used. By default the auto save is disabled.
+		/// @param maxLogEntries Define the maximum entries will be stored in memory. By default the value is 0, to keep all entries in memory. This feature is only used when auto save is enabled.
+		void setAutoSave(bool autoSave, size_t maxLogEntries = 0);
+};
+
+#endif // !LOGGER_HPP
