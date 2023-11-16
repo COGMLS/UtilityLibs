2eaaa0aff6f7bf54f1a2431f9eed3123cdd099e7
COGMLS <mlsmatheus@hotmail.com>
2023-11-16 15:52:43 -0300

    Added support to wide strings version details.

--- Libraries/SettingsLib/SettingsLibSolution/SettingsLib/SettingsLibProperties.hpp
+++ Libraries/SettingsLib/SettingsLibSolution/SettingsLib/SettingsLibProperties.hpp
@@ -8,9 +8,20 @@
 
 #define SETTINGS_LIBRARY_MAJOR_VERSION		1
 #define SETTINGS_LIBRARY_MINOR_VERSION		4
-#define SETTINGS_LIBRARY_REVISION_VERSION	15
+#define SETTINGS_LIBRARY_REVISION_VERSION	16
 #define SETTINGS_LIBRARY_BUILD_TYPE			"beta"
-#define SETTINGS_LIBRARY_BUILD_VERSION		20
+#define SETTINGS_LIBRARY_BUILD_TYPE_W		L"beta"
+#define SETTINGS_LIBRARY_BUILD_VERSION		21
+
+/// @brief Settings Library Version struct
+struct SettingsLibVersion
+{
+	unsigned int major;
+	unsigned int minor;
+	unsigned int revision;
+	char buildType[8];
+	unsigned int build;
+};
 
 /// @brief Get the Settings Library version string
 inline std::string getSettingsLibVersion()
@@ -22,6 +33,24 @@ inline std::string getSettingsLibVersion()
 inline std::string getSettingsLibBuild()
 {
 	return std::string("Build: ") + std::to_string(SETTINGS_LIBRARY_BUILD_VERSION);
+}
+
+/// @brief Get the Settings Library version wide string
+inline std::wstring getSettingsLibVersionW()
+{
+	return std::wstring(std::to_wstring(SETTINGS_LIBRARY_MAJOR_VERSION) + L"." + std::to_wstring(SETTINGS_LIBRARY_MINOR_VERSION) + L"." + std::to_wstring(SETTINGS_LIBRARY_REVISION_VERSION) + L"-" + SETTINGS_LIBRARY_BUILD_TYPE_W);
+}
+
+/// @brief Get the Settings Library build wide string
+inline std::wstring getSettingsLibBuildW()
+{
+	return std::wstring(L"Build: ") + std::to_wstring(SETTINGS_LIBRARY_BUILD_VERSION);
+}
+
+/// @brief Get all version and build information on struct SettingsLibVersion. NOTE: The build type is only given in ASCII.
+inline const SettingsLibVersion getSettingsLibVersionData()
+{
+	return SettingsLibVersion{ SETTINGS_LIBRARY_MAJOR_VERSION, SETTINGS_LIBRARY_MINOR_VERSION, SETTINGS_LIBRARY_REVISION_VERSION, SETTINGS_LIBRARY_BUILD_TYPE, SETTINGS_LIBRARY_BUILD_VERSION };
 }
 
 #endif // !SETTINGS_LIBRARY_PROPERTIES_HPP
