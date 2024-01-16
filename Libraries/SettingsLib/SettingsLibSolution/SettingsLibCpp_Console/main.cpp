c0b8043031aaa2c0d37bf77cfb06b75bf70d8114
COGMLS <mlsmatheus@hotmail.com>
2024-01-16 19:36:55 -0300

    Removed ConsoleTest. Updated main.cpp in SettingsLibCpp_Console.

--- Libraries/SettingsLib/SettingsLibSolution/SettingsLibCpp_Console/main.cpp
+++ Libraries/SettingsLib/SettingsLibSolution/SettingsLibCpp_Console/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 
 #include "SettingsLibDef.hpp"
+#include "SettingsLibProperties.hpp"
 #include "SettingsLib.hpp"
+
+#define FIELD1		"Field1"
+#define FIELD1W		L"Field1"
 
 int main(int argc, const char* argv[])
 {
+	std::cout << "Settings Library Test\n" << std::endl;
+
+	std::cout << "Library version: " << SettingsLib::getSettingsLibVersion() << std::endl;
+
+	SettingsLib::SettingsLibVersion settingsVer = SettingsLib::getSettingsLibVersionData();
+
+	std::cout << "\nMajor ver: \t" << settingsVer.major
+		<< "\nMinor ver: \t" << settingsVer.minor
+		<< "\nRevision: \t" << settingsVer.revision
+		<< "\nBuild Type: \t" << settingsVer.buildType
+		<< "\nBuild: \t\t" << settingsVer.build
+		<< "\n" << std::endl;
+
 	return 0;
 }
