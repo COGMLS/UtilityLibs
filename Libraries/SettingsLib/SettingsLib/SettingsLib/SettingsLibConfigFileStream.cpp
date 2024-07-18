b176319ef7fefe2042417ad575fac97bb1f8de19
COGMLS <mlsmatheus@hotmail.com>
2024-07-18 02:24:38 -0300

    Moved ConfigFileStream to sub-namespace Types. Changed pointers to use unique_ptr. Updated ConfigFileStream documentation. Added new methods to control the ConfigFileStream.

--- Libraries/SettingsLib/SettingsLib/SettingsLib/SettingsLibConfigFileStream.cpp
+++ Libraries/SettingsLib/SettingsLib/SettingsLib/SettingsLibConfigFileStream.cpp
@@ -1,10 +1,59 @@
 #include "SettingsLibConfigFileStream.hpp"
 
-SettingsLib::ConfigFileStream::ConfigFileStream()
+int SettingsLib::Types::ConfigFileStream::save2Store()
+{
+	if (this->keepCfgStore)
+	{
+		if (this->isWideData)
+		{
+			try
+			{
+				// Store WFstream to Vector Memory:
+				//int sWFs2Vmem = SettingsLib::Tools::storeFstream2Memory(this->wCfgFileStream.get(), this->wCfgStore.get(), 0, true, true);
+
+				//if (sWFs2Vmem != 0)
+				//{
+				//	this->errorsList->push_back(std::exception(std::string("Fail to read into the memory the wCfgFileStream. Return " + std::to_string(sWFs2Vmem)).c_str()));
+				//}
+
+				return 1;
+			}
+			catch(const std::exception& e)
+			{
+				this->errorsList->push_back(e);
+				return -1;
+			}
+		}
+		else
+		{
+			try
+			{
+				// Store Fstream to Vector Memory:
+				int sFs2Vmem = SettingsLib::Tools::storeFstream2Memory(this->cfgFileStream.get(), this->cfgStore.get(), 0, true, true);
+
+				if (sFs2Vmem != 0)
+				{
+					this->errorsList->push_back(std::exception(std::string("Fail to read into the memory the cfgFileStream. Return " + std::to_string(sFs2Vmem)).c_str()));
+				}
+
+				return 1;
+			}
+			catch(const std::exception& e)
+			{
+				this->errorsList->push_back(e);
+				return -1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+SettingsLib::Types::ConfigFileStream::ConfigFileStream()
 {
 }
 
-SettingsLib::ConfigFileStream::ConfigFileStream(std::filesystem::path cfgFilePath, bool useWideData, bool useReadonly, bool keepCfgStore)
+SettingsLib::Types::ConfigFileStream::ConfigFileStream(std::filesystem::path cfgFilePath, bool useWideData, bool useReadonly, bool keepCfgStore)
 {
 	this->cfgFilePath = cfgFilePath;
 	this->isWideData = useWideData;
@@ -12,7 +61,7 @@ SettingsLib::ConfigFileStream::ConfigFileStream(std::filesystem::path cfgFilePat
 	this->keepCfgStore = keepCfgStore;
 
 	this->isNewFile = false;
-	this->errorsList = new std::vector<std::exception>;
+	this->errorsList.reset(new std::vector<std::exception>);
 
 	if (!std::filesystem::exists(this->cfgFilePath))
 	{
@@ -30,54 +79,420 @@ SettingsLib::ConfigFileStream::ConfigFileStream(std::filesystem::path cfgFilePat
 			this->cfgFileMode = std::ios_base::in | std::ios_base::out;
 		}
 
-		if (this->isWideData)
-		{
-			// Not ready the wide stream
-		}
-		else
-		{
-			this->cfgFileStream = new std::fstream;
-
-			try
-			{
-				this->cfgFileStream->open(this->cfgFilePath.string(), this->cfgFileMode);
-
-				if (this->cfgFileStream->is_open())
-				{
-					if (this->keepCfgStore)
-					{
-						// Store Fstream to Vector Memory:
-						int sFs2Vmem = SettingsLib::Tools::storeFstream2Memory(this->cfgFileStream, this->cfgStore, 0, true, true);
-
-						if (sFs2Vmem == 0)
-						{
-							// Use the vector to analyze the lines:
-							// Call function to analyze the line
-						}
-						else
-						{
-							this->errorsList->push_back(std::exception(std::string("Fail to read into the memory the cfgFileStream. Return " + std::to_string(sFs2Vmem)).c_str()));
-						}
-					}
-					else
-					{
-						
-					}
-				}
-				else
-				{
-					this->errorsList->push_back(std::exception("Fail to open the configuration file!"));
-				}
-			}
-			catch(const std::exception& e)
-			{
-				this->errorsList->push_back(e);
-			}
-		}
+		this->openConfigStream(keepCfgStore, useReadonly);
 	}
 	else
 	{
 		this->isCfgFileOk = false;
 		this->errorsList->push_back(std::exception("cfgFilePath does not lead to a configuration file!"));
 	}
+}
+
+SettingsLib::Types::ConfigFileStream::~ConfigFileStream()
+{
+	this->closeConfigStream();
+	this->freeCfgStore();
+
+	if (this->cfgFileStream != nullptr)
+	{
+		this->cfgFileStream.reset(nullptr);
+	}
+
+	if (this->wCfgFileStream != nullptr)
+	{
+		this->wCfgFileStream.reset(nullptr);
+	}
+
+	if (this->cfgStore != nullptr)
+	{
+		this->cfgStore.reset(nullptr);
+	}
+
+	if (this->wCfgStore != nullptr)
+	{
+		this->wCfgStore.reset(nullptr);
+	}
+
+	if (this->errorsList != nullptr)
+	{
+		this->errorsList.reset(nullptr);
+	}
+}
+
+void SettingsLib::Types::ConfigFileStream::closeConfigStream()
+{
+	if (this->isWideData)
+	{
+		this->wCfgFileStream->close();
+	}
+	else
+	{
+		this->cfgFileStream->close();
+	}
+}
+
+bool SettingsLib::Types::ConfigFileStream::openConfigStream(bool refreshCfgStore, bool useReadonly)
+{
+	bool successfullyOpened = false;
+
+	if (this->isReadonly)
+	{
+		this->cfgFileMode = std::ios_base::in;
+	}
+	else
+	{
+		this->cfgFileMode = std::ios_base::in | std::ios_base::out;
+	}
+
+	// To refresh the store, set to false to free automatically the memory:
+	if (this->keepCfgStore && refreshCfgStore)
+	{
+		this->setKeepCfgStore(false);
+	}
+
+	this->setKeepCfgStore(refreshCfgStore);
+
+	if (this->isWideData)
+	{
+		if (wCfgFileStream == nullptr)
+		{
+			this->wCfgFileStream.reset(new std::wfstream);
+		}
+
+		try
+		{
+			this->wCfgFileStream->open(this->cfgFilePath.wstring(), this->cfgFileMode);
+			successfullyOpened = this->wCfgFileStream->is_open();
+
+			if (successfullyOpened)
+			{
+				this->save2Store();
+			}
+		}
+		catch(const std::exception& e)
+		{
+			this->errorsList->push_back(e);
+		}
+	}
+	else
+	{
+		if (this->cfgFileStream == nullptr)
+		{
+			this->cfgFileStream.reset(new std::fstream);
+		}
+
+		try
+		{
+			this->cfgFileStream->open(this->cfgFilePath.string(), this->cfgFileMode);
+			successfullyOpened = this->cfgFileStream->is_open();
+
+			if (successfullyOpened)
+			{
+				this->save2Store();
+			}
+		}
+		catch(const std::exception& e)
+		{
+			this->errorsList->push_back(e);
+		}
+	}
+
+	// Report is the opening file fail:
+	if (!successfullyOpened)
+	{
+		this->errorsList->push_back(std::exception("Fail to open the configuration file!"));
+	}
+
+    return successfullyOpened;
+}
+
+bool SettingsLib::Types::ConfigFileStream::isConfigStreamOpen()
+{
+	if (this->isWideData)
+	{
+		return this->wCfgFileStream->is_open();
+	}
+	else
+	{
+		return this->cfgFileStream->is_open();
+	}
+}
+
+int SettingsLib::Types::ConfigFileStream::getConfigLines(std::vector<std::string> *vMemStore)
+{
+	if (vMemStore == nullptr)
+	{
+		return -2;
+	}
+	if (this->keepCfgStore)
+	{
+		if (this->isWideData)
+		{
+			return -1;
+		}
+		else
+		{
+			try
+			{
+				*vMemStore = *this->cfgStore.get();
+				return 1;
+			}
+			catch(const std::exception& e)
+			{
+				this->errorsList->push_back(e);
+				return -3;
+			}
+			
+		}
+	}
+
+    return 0;
+}
+
+int SettingsLib::Types::ConfigFileStream::getConfigLine(size_t nLine, std::string *line, bool useCfgStore)
+{
+	if (this->isConfigStreamOpen())
+	{
+		return 1;
+	}
+
+	if (line == nullptr)
+	{
+		return 2;
+	}
+
+	if (useCfgStore && !this->keepCfgStore)
+	{
+		return 6;
+	}
+
+	if (useCfgStore)
+	{
+		if (this->isWideData)
+		{
+			return 5;
+		}
+		else
+		{
+			if (this->cfgStore->size() > nLine)
+			{
+				return 4;
+			}
+
+			try
+			{
+				// Get the config store data:
+				*line = this->cfgStore->at(nLine);
+				return 0;
+			}
+			catch(const std::exception& e)
+			{
+				return 3;
+			}
+		}
+	}
+	else
+	{
+		if (this->isWideData)
+		{
+			return 5;
+		}
+		else
+		{
+			// Set to the begin of the file:
+			this->cfgFileStream->seekg(std::fstream::beg);
+			this->cfgFileStream->seekp(std::fstream::beg);
+
+			size_t i = 0;
+			std::string lLine;
+			while (std::getline(*this->cfgFileStream.get(), lLine))
+			{
+				if (i == nLine)
+				{
+					*line = lLine;
+					break;
+				}
+
+				lLine.clear();
+				i++;
+			}
+
+			return 0;
+		}
+	}
+}
+
+int SettingsLib::Types::ConfigFileStream::getConfigLine(size_t nLine, std::wstring *line, bool useCfgStore)
+{
+	if (this->isConfigStreamOpen())
+	{
+		return 1;
+	}
+
+	if (line == nullptr)
+	{
+		return 2;
+	}
+
+	if (useCfgStore && !this->keepCfgStore)
+	{
+		return 6;
+	}
+
+	if (useCfgStore)
+	{
+		if (this->isWideData)
+		{
+			if (this->wCfgStore->size() > nLine)
+			{
+				return 4;
+			}
+
+			try
+			{
+				// Get the config store data:
+				*line = this->wCfgStore->at(nLine);
+				return 0;
+			}
+			catch(const std::exception& e)
+			{
+				return 3;
+			}
+		}
+		else
+		{
+			return 5;
+		}
+	}
+	else
+	{
+		if (this->isWideData)
+		{
+			// Set to the begin of the file:
+			this->wCfgFileStream->seekg(std::wfstream::beg);
+			this->wCfgFileStream->seekp(std::wfstream::beg);
+
+			size_t i = 0;
+			std::wstring lLine;
+			while (std::getline(*this->wCfgFileStream.get(), lLine))
+			{
+				if (i == nLine)
+				{
+					*line = lLine;
+					break;
+				}
+
+				lLine.clear();
+				i++;
+			}
+
+			return 0;
+		}
+		else
+		{
+			return 5;
+		}
+	}
+}
+
+void SettingsLib::Types::ConfigFileStream::getConfigStream(std::fstream *cfgFileStream)
+{
+	cfgFileStream = this->cfgFileStream.get();
+}
+
+size_t SettingsLib::Types::ConfigFileStream::getvMemStoreSize()
+{
+	if (this->cfgStore != nullptr || this->wCfgStore != nullptr)
+	{
+		if (this->isWideData)
+		{
+			return this->wCfgStore->size();
+		}
+		else
+		{
+			return this->cfgStore->size();
+		}
+	}
+
+	return 0;
+}
+
+int SettingsLib::Types::ConfigFileStream::freeCfgStore()
+{
+	if (this->keepCfgStore)
+	{
+		if (this->isWideData)
+		{
+			if (this->wCfgStore != nullptr)
+			{
+				this->wCfgStore.reset(nullptr);
+				return 1;
+			}
+		}
+		else
+		{
+			if (this->cfgStore != nullptr)
+			{
+				this->cfgStore.reset(nullptr);
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+
+    return -1;
+}
+
+int SettingsLib::Types::ConfigFileStream::refreshCfgStore()
+{
+	// Verify if the config stream is accessible and save it into store:
+	if (this->isConfigStreamOpen())
+	{
+		this->save2Store();
+	}
+}
+
+bool SettingsLib::Types::ConfigFileStream::getKeepCfgStore()
+{
+    return this->keepCfgStore;
+}
+
+void SettingsLib::Types::ConfigFileStream::setKeepCfgStore(bool keepCfgStore)
+{
+	// If the vector store is enabled, delete the data:
+	if (this->keepCfgStore && !keepCfgStore)
+	{
+		this->freeCfgStore();
+	}
+
+	// If the vector store is disabled, create the data:
+	if (!this->keepCfgStore && keepCfgStore)
+	{
+		if (this->isWideData)
+		{
+			this->wCfgStore.reset(new std::vector<std::wstring>);
+		}
+		else
+		{
+			this->cfgStore.reset(new std::vector<std::string>);
+		}
+	}
+
+	this->keepCfgStore = keepCfgStore;
+}
+
+bool SettingsLib::Types::ConfigFileStream::isReadonlyMode()
+{
+    return this->isReadonly;
+}
+
+std::vector<std::exception> SettingsLib::Types::ConfigFileStream::getErrorList()
+{
+	if (this->errorsList != nullptr)
+	{
+		return *this->errorsList.get();
+	}
+
+    return std::vector<std::exception>();
 }
--- Libraries/SettingsLib/SettingsLib/SettingsLib/SettingsLibConfigFileStream.hpp
+++ Libraries/SettingsLib/SettingsLib/SettingsLib/SettingsLibConfigFileStream.hpp
@@ -16,6 +16,7 @@
 
 #include <filesystem>
 #include <fstream>
+#include <memory>
 #include <string>
 #include <vector>
 #include <stdexcept>
@@ -27,161 +28,213 @@
 
 namespace SettingsLib
 {
-	/**
-	 * @brief Configuration File Stream to manage the file manipulation.
-	 */
-	class SETTINGS_LIB_API ConfigFileStream
+	namespace Types
 	{
-		protected:
-			// Configuration file path:
-			std::filesystem::path cfgFilePath;
+		/**
+		 * @brief Configuration File Stream to manage the file manipulation.
+		 */
+		class SETTINGS_LIB_API ConfigFileStream
+		{
+			protected:
+				// Configuration file path:
+				std::filesystem::path cfgFilePath;
 
-			// Configuration file stream:
-			std::fstream* cfgFileStream = nullptr;
-			std::wfstream* wCfgFileStream = nullptr;
-			std::ios_base::openmode cfgFileMode;
+				//
+				// Configuration file stream:
+				//
 
-			// Configuration local memory store:
-			std::vector<std::string>* cfgStore = nullptr;
-			std::vector<std::wstring>* wCfgStore = nullptr;
+				std::unique_ptr<std::fstream> cfgFileStream;
+				std::unique_ptr<std::wfstream> wCfgFileStream;
+				std::ios_base::openmode cfgFileMode;
 
-			// Variable controls:
-			bool isWideData;
-			bool isReadonly;
-			bool isNewFile;
-			bool isCfgFileOk;
-			bool keepCfgStore;
+				//
+				// Configuration local memory store:
+				//
 
-			// Exception history:
-			std::vector<std::exception>* errorsList = nullptr;
+				std::unique_ptr<std::vector<std::string>> cfgStore;
+				std::unique_ptr<std::vector<std::wstring>> wCfgStore;
 
-		public:
-			/// @brief This constructor is only for inheritance.
-			ConfigFileStream();
+				//
+				// Variable controls:
+				//
 
-			/**
-			 * @brief Create an object to control the configuration file stream and it's lines
-			 * @param cfgFilePath Path to the configuration line.
-			 * @param useWideData Define to use wstrings for (UTF-16) format. This definition can't be changed after the object creation.
-			 * @param useReadonly Define if it will block for changes in file stream. This definition can't be changed after the object creation.
-			 * @param keepCfgStore Define to keep the configuration vector memory stored or clean after the reading file operation.
-			 * @note Use wide data (UTF-16) is not recomended.
-			 * @note Keeping the configuration lines in memory can allow easily verify the data was changed for some applications but can use unnecessary memory depending on the number of lines.
-			 * @note When freeCfgStore is called, the memory is cleaned. To have access again to memory store, call refreshCfgStore.
-			 * @note To access the configuration lines when keepCfgStore is not set, is necessary call extractLines. It's possible access directly the file stream but isn't recomended because it's not possible to control if the stream will be closed or changed.
-			 */
-			ConfigFileStream (std::filesystem::path cfgFilePath, bool useWideData, bool useReadonly, bool keepCfgStore);
+				bool isWideData;
+				bool isReadonly;
+				bool isNewFile;
+				bool isCfgFileOk;
+				bool keepCfgStore;
 
-			/**
-			 * @brief Close the file stream.
-			 * @note This method only close the file stream and doesn't free the vector confguration store.
-			 */
-			void closeConfigStream();
+				// Exception history:
+				std::unique_ptr<std::vector<std::exception>> errorsList;
 
-			/**
-			 * @brief Reopen the configuration file stream
-			 * @return True if a successful operation occur.
-			 */
-			bool openConfigStream();
+				//
+				// Internal methods:
+				//
 
-			/**
-			 * @brief Check if the configuration file stream is opened.
-			 */
-			bool isConfigStreamOpen();
+				/**
+				 * @brief Save the configuration lines into the Vector
+				 * @return Return -1 if a exception was found
+				 * @return Return 0 if keepCfgStore is false
+				 * @return Return 1 if the data line was successfully saved into the vector
+				 */
+				int save2Store();
 
-			/**
-			 * @brief Get the configuration lines stored in memory
-			 * @param vMemStore Pointer to receive the access to the vector
-			 * @return Return the value from Tools::storeFstream2Memory function
-			 */
-			int getConfigLines (std::vector<std::string>* vMemStore);
+			public:
+				/// @brief This constructor is only for inheritance.
+				ConfigFileStream();
 
-			/**
-			 * @brief Get a configuration line stored in memory
-			 * @param nLine Line number that is compatible with the vector's index.
-			 * @param line Pointer to receive the access to the string line
-			 * @return Return the value from Tools::storeFstream2Memory function
-			 * @note If nLine receive a number bigger than the vector size, the function will return 5.
-			 */
-			int getConfigLine (unsigned long long nLine, std::string* line);
+				/**
+				 * @brief Create an object to control the configuration file stream and it's lines
+				 * @param cfgFilePath Path to the configuration line.
+				 * @param useWideData Define to use wstring for (UTF-16) format. This definition can't be changed after the object creation.
+				 * @param useReadonly Define if it will block for changes in file stream. This definition can't be changed after the object creation.
+				 * @param keepCfgStore Define to keep the configuration vector memory stored or clean after the reading file operation.
+				 * @note Use wide data (UTF-16) is not recommended.
+				 * @note Keeping the configuration lines in memory can allow easily verify the data was changed for some applications but can use unnecessary memory depending on the number of lines.
+				 * @note When freeCfgStore is called, the memory is cleaned. To have access again to memory store, call refreshCfgStore.
+				 * @note To access the configuration lines when keepCfgStore is not set, is necessary call extractLines. It's possible access directly the file stream but isn't recommended because it's not possible to control if the stream will be closed or changed.
+				 */
+				ConfigFileStream (std::filesystem::path cfgFilePath, bool useWideData, bool useReadonly, bool keepCfgStore);
 
-			/**
-			 * @brief Access the configuration file stream.
-			 * @param cfgFileStream 
-			 * @return 
-			 * @warning The configuration stream object can't control how the file stream is used outside. Carefull when using this method!
-			 */
-			int getConfigStream (std::fstream* cfgFileStream);
+				/**
+				 * @brief Destroy the configuration file stream.
+				 */
+				~ConfigFileStream();
 
-			/**
-			 * @brief Set the configuration lines to store in memory and be able to save into the file
-			 * @param new_vMemStore Vector with configuration lines
-			 * @return 
-			 */
-			int setConfigLines (std::vector<std::string>* new_vMemStore);
+				/**
+				 * @brief Close the file stream.
+				 * @note This method only close the file stream and doesn't free the vector configuration store.
+				 */
+				void closeConfigStream();
 
-			/**
-			 * @brief Set a configuration line in memory. It's possible to change it or add the line.
-			 * @param lineN Line number
-			 * @param line String repreesnting the configuration line
-			 * @param overWrite If true will change the actual line. Otherwise, will add a new line in lineN position.
-			 * @return 
-			 * @note If lineN is bigger than vMemStore, the line will be inserted in the end.
-			 */
-			int setConfigLine (unsigned long long lineN, std::string* line, bool overWrite);
+				/**
+				 * @brief Reopen the configuration file stream
+				 * @param refreshCfgStore Refresh configuration memory, if previous enabled
+				 * @param useReadonly Define or redefine open mode
+				 * @return True if a successful operation occur.
+				 */
+				bool openConfigStream(bool refreshCfgStore, bool useReadonly);
 
-			/**
-			 * @brief Get the size of the vector that is stored in memory with the configuration lines.
-			 * @note Zero can represent an empty vector or that keepCfgStore was disable. Check the keepCfgStore status with getKeepCfgStore method.
-			 */
-			size_t getvMemStoreSize();
+				/**
+				 * @brief Check if the configuration file stream is opened.
+				 */
+				bool isConfigStreamOpen();
 
-			/**
-			 * @brief Free the memory from configuration lines that was stored.
-			 * @return 0 if the memory was freed.
-			 * @return 1 if the memory was already free.
-			 */
-			int freeCfgStore();
+				/**
+				 * @brief Get the configuration lines stored in memory
+				 * @param vMemStore Pointer to receive the access to the vector
+				 * @return Return the value from Tools::storeFstream2Memory function
+				 */
+				int getConfigLines (std::vector<std::string>* vMemStore);
 
-			/**
-			 * @brief Refresh the configuration lines store in memory
-			 * @return Return the value from Tools::storeFstream2Memory function
-			 * @note If memory was free, it will reallocate to store again.
-			 * @note If keepCfgStore is not set, will return 5.
-			 */
-			int refreshCfgStore();
+				/**
+				 * @brief Get a configuration line stored in memory or read directly from the file stream.
+				 * @param nLine Line number that is compatible with the vector's index or line number from file stream.
+				 * @param line Pointer to receive the access to the string line.
+				 * @param useCfgStore Define to read from configuration stored in memory or use directly from file stream.
+				 * @return Return 0 if the operation was completed with success.
+				 * @return Return 1 if the configuration file stream is not open.
+				 * @return Return 2 if line is nullptr.
+				 * @return Return 3 if an unknown exception occur.
+				 * @return Return 4 in case nLine is bigger than vector's size.
+				 * @return Return 5 in case the object is configured to use wstring.
+				 * @return Return 6 when useCfgStore is true but keep the configuration lines into memory is disabled.
+				 */
+				int getConfigLine (size_t nLine, std::string* line, bool useCfgStore);
 
-			/**
-			 * @brief Refresh the configuration lines store in memory
-			 * @param externCfgStore External configuration line store.
-			 * @return Return the value from Tools::storeFstream2Memory function
-			 */
-			int refreshCfgStore(std::vector<std::string>* externCfgStore);
+				/**
+				 * @brief Get a configuration line stored in memory or read directly from the file stream.
+				 * @param nLine Line number that is compatible with the vector's index or line number from file stream.
+				 * @param line Pointer to receive the access to the string line.
+				 * @param useCfgStore Define to read from configuration stored in memory or use directly from file stream.
+				 * @return Return 0 if the operation was completed with success.
+				 * @return Return 1 if the configuration file stream is not open.
+				 * @return Return 2 if line is nullptr.
+				 * @return Return 3 if an unknown exception occur.
+				 * @return Return 4 in case nLine is bigger than vector's size.
+				 * @return Return 5 in case the object is configured to use string.
+				 * @return Return 6 when useCfgStore is true but keep the configuration lines into memory is disabled.
+				 */
+				int getConfigLine (size_t nLine, std::wstring* line, bool useCfgStore);
 
-			/**
-			 * @brief Get the status of configuration memory store
-			 */
-			bool getKeepCfgStore();
+				/**
+				 * @brief Access the configuration file stream.
+				 * @param cfgFileStream  
+				 * @warning The configuration stream object can't control how the file stream is used outside. Careful when using this method!
+				 */
+				void getConfigStream (std::fstream* cfgFileStream);
 
-			/**
-			 * @brief Define the keepCfgStore setting.
-			 * @param keepCfgStore Value to define the configuration memory store behavior.
-			 * @note Disabling keepCfgStore, will call freeCfgStore automatically
-			 */
-			void setKeepCfgStore (bool keepCfgStore);
+				/**
+				 * @brief Set the configuration lines to store in memory and be able to save into the file
+				 * @param new_vMemStore Vector with configuration lines
+				 * @return 
+				 */
+				int setConfigLines (std::vector<std::string>* new_vMemStore, bool overwrite);
 
-			/**
-			 * @brief Check if the configuration file stream accept changes.
-			 * @note If the file was created during the object creation and readonly was defined, the property will be disabled to allow write access to file stream. 
-			 */
-			bool isReadonlyMode();
+				/**
+				 * @brief Set a configuration line in memory. It's possible to change it or add the line.
+				 * @param lineN Line number
+				 * @param line String representing the configuration line
+				 * @param overwrite If true will change the actual line. Otherwise, will add a new line in lineN position.
+				 * @return 
+				 * @note If lineN is bigger than vMemStore, the line will be inserted in the end.
+				 */
+				int setConfigLine (size_t lineN, std::string* line, bool overwrite);
 
-			/**
-			 * @brief Access the error list. If no error or status was created, an empty vector will return.
-			 * @return Return a vector of exceptions founded.
-			 */
-			std::vector<std::exception> getErrorList();
-	};
+				/**
+				 * @brief Get the size of the vector that is stored in memory with the configuration lines.
+				 * @note Zero can represent an empty vector or that keepCfgStore was disable. Check the keepCfgStore status with getKeepCfgStore method.
+				 */
+				size_t getvMemStoreSize();
+
+				/**
+				 * @brief Free the memory from configuration lines that was stored.
+				 * @return -1 if the keepCfgStore is disabled.
+				 * @return 0 if the memory was freed.
+				 * @return 1 if the memory was already free.
+				 */
+				int freeCfgStore();
+
+				/**
+				 * @brief Refresh the configuration lines store in memory
+				 * @return Return the value from Tools::storeFstream2Memory function
+				 * @note If memory was free, it will reallocate to store again.
+				 * @note If keepCfgStore is not set, will return 5.
+				 */
+				int refreshCfgStore();
+
+				/**
+				 * @brief Refresh the configuration lines store in memory
+				 * @param externCfgStore External configuration line store.
+				 * @return Return the value from Tools::storeFstream2Memory function
+				 */
+				int refreshCfgStore(std::vector<std::string>* externCfgStore);
+
+				/**
+				 * @brief Get the status of configuration memory store
+				 */
+				bool getKeepCfgStore();
+
+				/**
+				 * @brief Define the keepCfgStore setting.
+				 * @param keepCfgStore Value to define the configuration memory store behavior.
+				 * @note Disabling keepCfgStore, will call freeCfgStore automatically
+				 */
+				void setKeepCfgStore (bool keepCfgStore);
+
+				/**
+				 * @brief Check if the configuration file stream accept changes.
+				 * @note If the file was created during the object creation and readonly was defined, the property will be disabled to allow write access to file stream. 
+				 */
+				bool isReadonlyMode();
+
+				/**
+				 * @brief Access the error list. If no error or status was created, an empty vector will return.
+				 * @return Return a vector of exceptions founded.
+				 */
+				std::vector<std::exception> getErrorList();
+		};
+	}
 }
 
 #endif //! SETTINGS_LIB_CONFIG_FILE_STREAM_HPP
