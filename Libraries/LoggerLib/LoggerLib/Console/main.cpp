40bb71585c84bbd6c4a06cd9893076fe548b4bee
COGMLS <mlsmatheus@hotmail.com>
2024-10-08 10:49:41 -0300

    Update LoggerLib to 3.1.2-release Changed the getVersionStr parameter to accept an copy and not the variable address any more. Added missing PATCH number. Added better const char[] conversion to char*. Fixed version string conversion when BUILD_TYPE is marked as "release". Fixed revision number position when converting version to string. New tests with LoggerLib.

--- Libraries/LoggerLib/LoggerLib/Console/main.cpp
+++ Libraries/LoggerLib/LoggerLib/Console/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
-#include "LoggerLibDef.hpp"
+#include "LoggerLibVersionTools.hpp"
 #include "Logger.hpp"
-
-//#include "../LoggerLib/Logger.hpp
 
 int main()
 {
+	std::cout << "Logger Library Console Test - " << LoggerLib::getVersionStr(LoggerLib::getLibVersion(), true, true) << std::endl;
+	std::cout << "-------------------------------------------------------" << std::endl;
+
 	Logger logger(std::filesystem::current_path(), "logTest");
 
 	std::cout << "Created log " << logger.getLogFilePath().filename().string() << std::endl;
 
 	logger.newEntry(LogEntry("Test", "This is a log test"));
 	logger.newEntry(LogEntry("NewTest::", "This is a new test."));
+	logger.newEntry(LogEntry("LoggerLib version: ", LoggerLib::getVersionStr(LoggerLib::getLibVersion(), true, true)));
 	
 	int logSavStatus = logger.saveLog();
 	
