7f2a2f4399b492bf62ab5daacf858662887359be
COGMLS <mlsmatheus@hotmail.com>
2024-11-07 17:20:44 -0300

    Updated main library header SettingsLib.hpp

--- Libraries/SettingsLib/SettingsLib/SettingsLib/SettingsLib.hpp
+++ Libraries/SettingsLib/SettingsLib/SettingsLib/SettingsLib.hpp
@@ -5,10 +5,26 @@
 
 // Main Settings Library header
 
-#include "SettingsLibDefinitions.hpp"
-#include "SettingsLibDataTypes.hpp"
-#include "SettingsLibTools.hpp"
 #include "SettingsLibConfig.hpp"
+#include "SettingsLibConfigFileStream.hpp"
+#include "SettingsLibDataTypes.hpp"
+#include "SettingsLibDefinitions.hpp"
+#include "SettingsLibErrorCodes.hpp"
+#include "SettingsLibTools.hpp"
 #include "SettingsLibVersionHelper.hpp"
+
+// Components:
+
+// Exception:
+
+#include "Components/Exceptions/SettingsLibException.hpp"
+
+// Ini Components:
+
+#include "Components/Configs/Ini/SettingsIniConfig.hpp"
+#include "Components/Configs/Ini/SettingsIniConfigData.hpp"
+#include "Components/Configs/Ini/SettingsIniDefinitions.hpp"
+#include "Components/Configs/Ini/SettingsIniErrorCodes.hpp"
+#include "Components/Configs/Ini/SettingsIniTools.hpp"
 
 #endif // !SETTINGS_LIB_HPP
