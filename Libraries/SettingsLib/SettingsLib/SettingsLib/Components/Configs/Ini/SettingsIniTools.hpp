6d6858523c13730c0acbe418769f4f4621b25588
COGMLS <mlsmatheus@hotmail.com>
2024-11-07 17:16:45 -0300

    Moved Ini components to Components/Configs/Ini location. Added first support for cross-platform compilation.

--- Libraries/SettingsLib/SettingsLib/SettingsLib/Components/Configs/Ini/SettingsIniTools.hpp
+++ Libraries/SettingsLib/SettingsLib/SettingsLib/Components/Configs/Ini/SettingsIniTools.hpp
@@ -12,10 +12,25 @@
 
 	#pragma warning (disable : 4251)
 	#pragma warning (disable : 4273)
+#else
+	#if __GNUC__ >= 4
+		#ifdef SETTINGS_LIB_EXPORTS
+			#define SETTINGS_LIB_API __attribute__((visibility("default")))
+		#else
+			#define SETTINGS_LIB_API __attribute__((visibility("default")))
+		#endif //!SETTINGS_LIB_EXPORTS
+	#else
+		#ifdef SETTINGS_LIB_EXPORTS
+			#define SETTINGS_LIB_API
+		#else
+			#define SETTINGS_LIB_API
+		#endif //!SETTINGS_LIB_EXPORTS
+	#endif
 #endif // !WIN32
 
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <filesystem>
 #include <vector>
 #include <stdexcept>
