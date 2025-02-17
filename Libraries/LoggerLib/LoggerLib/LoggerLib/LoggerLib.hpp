1350794996fa02993fc824b3b467cab110da76ab
COGMLS <mlsmatheus@hotmail.com>
2025-02-17 18:32:15 -0300

    Added preprocessor guard checking for experimental headers.

--- Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerLib.hpp
+++ Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerLib.hpp
@@ -3,10 +3,24 @@
 #ifndef LOGGER_LIBRARY_HPP
 #define LOGGER_LIBRARY_HPP
 
+#include "LoggerExperimental.hpp"
+
+#ifdef LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+#include "LogDataStore.hpp"
+#endif // !LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+
+#ifdef LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+#include "LoggerInfoFormatter.hpp"
+#endif // !LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+
 #include "Logger.hpp"
 #include "LoggerCommonDataTypes.hpp"
 #include "LoggerDateTime.hpp"
+
+#ifdef LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
 #include "LoggerException.hpp"
+#endif // !LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
+
 #include "LoggerLibVersion.hpp"
 #include "LoggerLibVersionTools.hpp"
 #include "LogMng.hpp"
