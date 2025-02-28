c4c05fd82f24e064848318722e4c0edffd0f95aa
COGMLS <mlsmatheus@hotmail.com>
2025-02-27 22:50:35 -0300

    Added BuildMetadata.hpp and BuildMetadata.cpp

---
+++ Libraries/VersionLib/VersionLib/VersionLib/BuildMetadata.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef VERSION_LIB_METADATA_HPP
+#define VERSION_LIB_METADATA_HPP
+
+#ifdef WIN32
+	#ifdef VERSION_LIB_EXPORTS
+		#define VERSION_LIB_API __declspec(dllexport)
+	#else
+		#define VERSION_LIB_API __declspec(dllimport)
+	#endif //!VERSION_LIB_EXPORTS
+
+	#pragma warning (disable : 4251)
+	#pragma warning (disable : 4273)
+#else
+	#if __GNUC__ >= 4
+		#ifdef VERSION_LIB_EXPORTS
+			#define VERSION_LIB_API __attribute__((visibility("default")))
+		#else
+			#define VERSION_LIB_API __attribute__((visibility("default")))
+		#endif //!VERSION_LIB_EXPORTS
+	#else
+		#ifdef VERSION_LIB_EXPORTS
+			#define VERSION_LIB_API
+		#else
+			#define VERSION_LIB_API
+		#endif //!VERSION_LIB_EXPORTS
+	#endif
+#endif // !WIN32
+
+#include "ExperimentalFeatures.hpp"
+
+namespace VersionLib
+{
+	
+}
+
+#endif // !VERSION_LIB_METADATA_HPP
