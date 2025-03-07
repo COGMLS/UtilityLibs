ecf096a24c4526f1d4df67adcaca6d9d535637b5
COGMLS <mlsmatheus@hotmail.com>
2025-03-07 17:06:15 -0300

    Revised auxiliary functions for VersionBuildTypeC to return true on success. Added PRE_ALPHA on BuildType enumerator.

--- Libraries/VersionLib/VersionLib/VersionLib/BuildTypes.cpp
+++ Libraries/VersionLib/VersionLib/VersionLib/BuildTypes.cpp
@@ -16,31 +16,86 @@ VersionLib::VersionBuildTypeC VERSION_LIB_API VersionLib::initVersionBuildTypeC(
 	return buildType;
 }
 
-void VersionLib::destroyVersionBuildTypeC(VersionLib::VersionBuildTypeC &build_type)
+bool VersionLib::destroyVersionBuildTypeC(VersionLib::VersionBuildTypeC &build_type)
 {
 	#ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
-	if (build_type.types != nullptr)
+	try
 	{
-		delete[] build_type.types;
-		build_type.types = nullptr;
-		build_type.size = 0;
+		if (build_type.types != nullptr)
+		{
+			delete[] build_type.types;
+			build_type.types = nullptr;
+			build_type.size = 0;
+		}
+		return true;
+	}
+	catch(const std::exception& e)
+	{
+		return false;
 	}
 	#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
-void VersionLib::setVersionBuildTypeC(VersionLib::VersionBuildTypeC &buildTypeStruct, VersionLib::BuildType build_type)
+bool VersionLib::setVersionBuildTypeC(VersionLib::VersionBuildTypeC &buildTypeStruct, VersionLib::BuildType build_type)
 {
 	#ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
-	if (buildTypeStruct.size != 1 || buildTypeStruct.types == nullptr)
+	try
 	{
-		destroyVersionBuildTypeC(buildTypeStruct);
-		buildTypeStruct.size = 1;
-		buildTypeStruct.types = new VersionLib::BuildType[buildTypeStruct.size];
+		if (buildTypeStruct.size != 1 || buildTypeStruct.types == nullptr)
+		{
+			destroyVersionBuildTypeC(buildTypeStruct);
+			buildTypeStruct.size = 1;
+			buildTypeStruct.types = new VersionLib::BuildType[buildTypeStruct.size];
+		}
+	
+		buildTypeStruct.types[0] = build_type;
+		return true;
 	}
-
-	buildTypeStruct.types[0] = build_type;
+	catch(const std::exception& e)
+	{
+		return false;
+	}
 	#else
 	buildTypeStruct.type = build_type;
+	return true;
 	#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
+
+#ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
+bool VersionLib::setVersionBuildTypeC(VersionLib::VersionBuildTypeC &buildTypeStruct, VersionLib::BuildType build_type[], unsigned short size)
+{
+    if (size == 0)
+	{
+		return false;
+	}
+
+	if (buildTypeStruct.types != nullptr)
+	{
+		VersionLib::destroyVersionBuildTypeC(buildTypeStruct);
+	}
+
+	try
+	{
+		if (buildTypeStruct.types != nullptr)
+		{
+			return false;
+		}
+
+		buildTypeStruct.size = size;
+		buildTypeStruct.types = new VersionLib::BuildType[buildTypeStruct.size];
+
+		for (unsigned int i = 0; i < buildTypeStruct.size; i++)
+		{
+			buildTypeStruct.types[i] = build_type[i];
+		}
+
+		return true;
+	}
+	catch(const std::exception& e)
+	{
+		return false;
+	}
+}
+#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
+
 #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_CLASS_BUILD_TYPE_COMPONENT
