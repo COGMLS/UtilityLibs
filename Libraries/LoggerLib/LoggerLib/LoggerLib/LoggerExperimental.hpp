5a22dabfd11a4daad49d8a457344a42162e8a7e5
COGMLS <mlsmatheus@hotmail.com>
2025-03-13 16:13:11 -0300

    Added few comments about the experimental definitions guards

--- Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerExperimental.hpp
+++ Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerExperimental.hpp
@@ -3,13 +3,28 @@
 #ifndef LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
 #define LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
 
+// Determinate to use LogDataStore class:
 #define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+
+// Determinate to use the experimental equality operator on LogDataStore:
 //#define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE_EXPERIMENTAL_EQ_OPERATOR // Permanently disabled. No tests with this experimental feature are necessary.
+
+// Determinate to use log data formatter:
 //#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+
+// Determinate to use custom exceptions:
 //#define LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
+
+// Set to use experimental high precision time on all platforms:
 //#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
+
+// Set to use experimental bubble sort alg v1:
 #define LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_1
+
+// Set to use experimental bubble sort alg v2:
 #define LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_2
+
+// Enable the experimental LogEntry date register:
 #define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
 
 #endif // !LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
