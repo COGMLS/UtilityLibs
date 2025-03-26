1f05574d04376313881d6bf7a5646d74a8aa93ea
COGMLS <mlsmatheus@hotmail.com>
2025-03-26 19:03:32 -0300

    Removed sortLogFileList2

--- Libraries/LoggerLib/LoggerLib/LoggerLib/LogMng.cpp
+++ Libraries/LoggerLib/LoggerLib/LoggerLib/LogMng.cpp
@@ -43,7 +43,6 @@ void sortLogFileList(std::vector<std::filesystem::path> &logFileList, bool newes
 			i++;
 		}
 
-		#ifdef LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_1
 		for (size_t i = 0; i < logFileList.size() - 1; i++)
 		{
 			swap = false;
@@ -62,104 +61,8 @@ void sortLogFileList(std::vector<std::filesystem::path> &logFileList, bool newes
 				break;
 			}
 		}
-		#else
-		for (i = 0; i < logPaths.size() - 1; i++)
-		{
-			swap = false;
-			min_index = i;
-
-			for (size_t j = i + 1; j < logPaths.size(); j++)
-			{
-				if ((newestFirst && logDtArr[i] < logDtArr[j]) || (!newestFirst && logDtArr[i] > logDtArr[j]))
-				{
-					min_index = j;
-					swap = true;
-				}
-			}
-
-			// Swap when found the last write time difference:
-			if (swap)
-			{
-				std::swap<std::filesystem::path>(logPaths[i], logPaths[min_index]);
-				std::swap<LogFileDateTime>(logDtArr[i], logDtArr[min_index]);
-			}
-		}
-		#endif // !LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_1
 
 		logFileList = logPaths;
-	}
-}
-
-void sortLogFileList2(std::vector<std::filesystem::path> &logFileList, bool newestFirst)
-{
-	if (!logFileList.empty())
-	{
-		bool swap = false;
-		size_t min_index = 0;
-		std::filesystem::file_time_type refLogTime;
-		std::filesystem::file_time_type logTime;
-
-		std::chrono::nanoseconds refLogTime_ns;
-		std::chrono::nanoseconds logTime_ns;
-
-		std::chrono::time_point now = std::chrono::utc_clock::now();
-
-		#ifdef LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_2
-		for (size_t i = 0; i < logFileList.size() - 1; i++)
-		{
-			swap = false;
-
-			refLogTime = std::filesystem::last_write_time(logFileList[i]);
-
-			for (size_t j = 0; j < logFileList.size() - i - 1; j++)
-			{
-				logTime = std::filesystem::last_write_time(logFileList[j]);
-
-				if ((newestFirst && refLogTime > logTime) || (!newestFirst && refLogTime < logTime))
-				{
-					std::swap(logFileList[j], logFileList[j + 1]);
-					swap = true;
-				}
-			}
-
-			if (!swap)
-			{
-				break;
-			}
-		}
-		#else
-		for (size_t i = 0; i < logFileList.size() - 1; i++)
-		{
-			swap = false;
-			min_index = i;
-			refLogTime = std::filesystem::last_write_time(logFileList[i]);
-			refLogTime_ns = now - std::chrono::clock_cast<std::chrono::utc_clock, std::chrono::file_clock>(refLogTime);
-
-			for (size_t j = i + 1; j < logFileList.size(); j++)
-			{
-				logTime = std::filesystem::last_write_time(logFileList[j]);
-				logTime_ns = now - std::chrono::clock_cast<std::chrono::utc_clock, std::chrono::file_clock>(logTime);
-
-				/*
-				 * If the refLog is older than actual log obj and newer was set
-				 * OR
-				 * When the refLog is newer than actual log and newestFirst was not set
-				 * mark to swap the objects:
-				*/
-				if ((newestFirst && refLogTime_ns > logTime_ns) || (!newestFirst && refLogTime_ns < logTime_ns))
-				{
-					min_index = j;
-					swap = true;
-				}
-			}
-
-			// Swap when found the last write time difference:
-			if (swap)
-			{
-				std::swap(logFileList[i], logFileList[min_index]);
-			}
-		}
-		#endif // !LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_2
 	}
 }
 
