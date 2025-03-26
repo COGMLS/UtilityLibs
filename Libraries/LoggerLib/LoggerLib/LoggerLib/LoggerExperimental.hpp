0f3cccebfc2a65ac108e8864ced6908912ea6404
COGMLS <mlsmatheus@hotmail.com>
2025-03-26 19:07:55 -0300

    Implemented build experimental feature controls

--- Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerExperimental.hpp
+++ Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerExperimental.hpp
@@ -3,6 +3,84 @@
 #ifndef LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
 #define LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
 
+#include "LoggerLibVersion.hpp"
+
+#ifdef LOGGER_LIB_USE_VERSION_TYPE_CONTROL
+
+//
+// Alpha Features:
+//
+
+#if LOGGER_LIB_COMPILE_BUILD_TYPE == 1
+	// Determinate to use log data formatter:
+	#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+
+	// Determinate to use custom exceptions:
+	#define LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
+
+	// Set to use experimental high precision time on all platforms:
+	#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
+
+	// Enable the experimental LogEntry date register:
+	#define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
+#endif // !LOGGER_LIB_COMPILE_BUILD_TYPE
+
+//
+// Beta Features:
+//
+
+#if LOGGER_LIB_COMPILE_BUILD_TYPE == 2
+	// Determinate to use log data formatter:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+
+	// Determinate to use custom exceptions:
+	#define LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
+
+	// Set to use experimental high precision time on all platforms:
+	#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
+
+	// Enable the experimental LogEntry date register:
+	#define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
+#endif // !LOGGER_LIB_COMPILE_BUILD_TYPE
+
+//
+// Release Candidate Features:
+//
+
+#if LOGGER_LIB_COMPILE_BUILD_TYPE == 3
+	// Determinate to use log data formatter:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+
+	// Determinate to use custom exceptions:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
+
+	// Set to use experimental high precision time on all platforms:
+	#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
+
+	// Enable the experimental LogEntry date register:
+	#define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
+#endif // !LOGGER_LIB_COMPILE_BUILD_TYPE
+
+//
+// Release Features:
+//
+
+#if LOGGER_LIB_COMPILE_BUILD_TYPE == 4
+	// Determinate to use log data formatter:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
+
+	// Determinate to use custom exceptions:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
+
+	// Set to use experimental high precision time on all platforms:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
+
+	// Enable the experimental LogEntry date register:
+	//#define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
+#endif // !LOGGER_LIB_COMPILE_BUILD_TYPE
+
+#else
+
 // Determinate to use LogDataStore class:
 #define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
 
@@ -15,13 +93,9 @@
 // Set to use experimental high precision time on all platforms:
 //#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
 
-// Set to use experimental bubble sort alg v1:
-#define LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_1
-
-// Set to use experimental bubble sort alg v2:
-#define LOGGER_ENABLE_EXPERIMENTAL_LOGGER_BUBBLE_SORT_2
-
 // Enable the experimental LogEntry date register:
 #define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
+
+#endif // !LOGGER_LIB_USE_VERSION_TYPE_CONTROL
 
 #endif // !LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
