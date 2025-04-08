3274296e8a719771fde44d0684567fb0d8392347
COGMLS <mlsmatheus@hotmail.com>
2025-04-08 20:57:09 -0300

    Removed unused old experimental feature section. Updated release candidate build. Added missing LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE

--- Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerExperimental.hpp
+++ Libraries/LoggerLib/LoggerLib/LoggerLib/LoggerExperimental.hpp
@@ -5,13 +5,14 @@
 
 #include "LoggerLibVersion.hpp"
 
-#ifdef LOGGER_LIB_USE_VERSION_TYPE_CONTROL
-
 //
 // Alpha Features:
 //
 
 #if LOGGER_LIB_COMPILE_BUILD_TYPE == 1
+	// Enable LogDataStore:
+	#define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+
 	// Determinate to use log data formatter:
 	#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
 
@@ -33,6 +34,9 @@
 //
 
 #if LOGGER_LIB_COMPILE_BUILD_TYPE == 2
+	// Enable LogDataStore:
+	#define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+
 	// Determinate to use log data formatter:
 	//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
 
@@ -54,6 +58,9 @@
 //
 
 #if LOGGER_LIB_COMPILE_BUILD_TYPE == 3
+	// Enable LogDataStore:
+	#define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+
 	// Determinate to use log data formatter:
 	//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
 
@@ -75,6 +82,9 @@
 //
 
 #if LOGGER_LIB_COMPILE_BUILD_TYPE == 4
+	// Enable LogDataStore:
+	#define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
+
 	// Determinate to use log data formatter:
 	//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
 
@@ -90,27 +100,5 @@
 	// Enable UTC and Local date time. This enable the local date time on Unix systems.
 	//#define LOGGER_ENABLE_EXPERIMENTAL_UTC_AND_LOCAL_DT
 #endif // !LOGGER_LIB_COMPILE_BUILD_TYPE
-
-#else
-
-// Determinate to use LogDataStore class:
-#define LOGGER_ENABLE_EXPERIMENTAL_DATA_STORE
-
-// Determinate to use log data formatter:
-//#define LOGGER_ENABLE_EXPERIMENTAL_DATA_FORMATTER
-
-// Determinate to use custom exceptions:
-//#define LOGGER_ENABLE_EXPERIMENTAL_CUSTOM_EXCEPTIONS
-
-// Set to use experimental high precision time on all platforms:
-//#define LOGGER_ENABLE_EXPERIMENTAL_ALL_PLATFORMS_HIGH_PRECISION_TIME
-
-// Enable the experimental LogEntry date register:
-#define LOGGER_ENABLE_EXPERIMENTAL_LOGENTRY_DT_REG
-
-// Enable UTC and Local date time. This enable the local date time on Unix systems.
-#define LOGGER_ENABLE_EXPERIMENTAL_UTC_AND_LOCAL_DT
-
-#endif // !LOGGER_LIB_USE_VERSION_TYPE_CONTROL
 
 #endif // !LOGGER_EXPERIMENTAL_FEATURES_CONTROL_HPP
