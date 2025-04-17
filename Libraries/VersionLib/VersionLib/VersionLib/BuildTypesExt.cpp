6560db1717abbcf58b78c275276951c54bbcb694
COGMLS <mlsmatheus@hotmail.com>
2025-04-17 16:20:49 -0300

    Changed VersionBuildType to use BuildRelease Added support to multiple release types in VersionBuildType

--- Libraries/VersionLib/VersionLib/VersionLib/BuildTypesExt.cpp
+++ Libraries/VersionLib/VersionLib/VersionLib/BuildTypesExt.cpp
@@ -2,153 +2,167 @@
 
 #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_CLASS_BUILD_TYPE_COMPONENT
 
-#ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 VersionLib::VersionBuildType::VersionBuildType()
 {
-    this->types.push_back(VersionLib::ALPHA);
+    this->types.push_back(VersionLib::BuildRelease());
 }
 
-VersionLib::VersionBuildType::VersionBuildType(std::vector<VersionLib::BuildType> combined_build_type)
+VersionLib::VersionBuildType::VersionBuildType(std::vector<VersionLib::BuildRelease> combined_build_type)
 {
     this->types = combined_build_type;
 }
 
 VersionLib::VersionBuildType::VersionBuildType(VersionLib::VersionBuildTypeC& combined_build_type)
 {
-    if (combined_build_type.types != nullptr || combined_build_type.size > 0)
+    if (combined_build_type.releases != nullptr || combined_build_type.size > 0)
     {
         for (size_t i = 0; i < combined_build_type.size; i++)
         {
-            this->types.push_back(combined_build_type.types[i]);
+            this->types.push_back(combined_build_type.releases[i]);
         }
     }
-}
-#else
-VersionLib::VersionBuildType::VersionBuildType()
-{
-	this->type = VersionLib::BuildType::ALPHA;
 }
 
 VersionLib::VersionBuildType::VersionBuildType(std::string build_type_str)
 {
-	this->type = VersionLib::str2BuildType(build_type_str);
+    if (build_type_str.empty())
+    {
+        this->types.push_back(VERSION_LIB_DEFAULT_BUILD_RELEASE_TYPE_INIT);
+    }
+    else
+    {
+        std::vector<VersionLib::VersionReleaseDataC> releases = VersionLib::findAndGetBuildTypes(build_type_str);
+        for (size_t i = 0; i < releases.size(); i++)
+        {
+            this->types.push_back(releases[i]);
+        }
+    }
 }
 
-VersionLib::VersionBuildType::VersionBuildType(VersionLib::BuildType build_type)
+VersionLib::VersionBuildType::VersionBuildType(VersionLib::BuildType build_type, unsigned short revision)
 {
-    this->type = build_type;
+    this->types.push_back(VersionLib::BuildRelease(build_type, revision));
 }
-#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 
 VersionLib::VersionBuildType::VersionBuildType(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     this->types = other.types;
-    #else
-    this->type = other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 VersionLib::VersionBuildType::VersionBuildType(VersionLib::VersionBuildType &&other) noexcept
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     this->types = std::move(other.types);
-    #else
-    this->type = std::move(other.type);
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 VersionLib::VersionBuildType::~VersionBuildType()
 {
 }
 
-#ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
-std::vector<VersionLib::BuildType> VersionLib::VersionBuildType::getBuildType()
+std::vector<VersionLib::BuildRelease> VersionLib::VersionBuildType::getBuildType()
 {
     return this->types;
 }
-#else
-VersionLib::BuildType VersionLib::VersionBuildType::getBuildType()
-{
-    return this->type;
-}
-#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 
-std::string VersionLib::VersionBuildType::getBuildTypeStr(bool useShortStr)
+std::string VersionLib::VersionBuildType::getBuildTypeStr(bool useShortStr, bool showReleaseType)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
-    return std::string();
-    #else
-    return VersionLib::buildType2Str(this->type, useShortStr);
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
+    std::string tmpBuildTypeStr;
+
+    for (size_t i = 0; i < this->types.size(); i++)
+    {
+        if (i == 0 || i + 1 < this->types.size())
+        {
+            tmpBuildTypeStr += VersionLib::buildType2Str(this->types[i].getRelease(), useShortStr, showReleaseType);
+        }
+        else
+        {
+            tmpBuildTypeStr += "." + VersionLib::buildType2Str(this->types[i].getRelease(), useShortStr, showReleaseType);
+        }
+
+        if (this->types[i].getRevision() > 0)
+        {
+            tmpBuildTypeStr += "." + std::to_string(this->types[i].getRevision());
+        }
+    }
+
+    return tmpBuildTypeStr;
 }
 
 VersionLib::VersionBuildTypeC VersionLib::VersionBuildType::toStruct()
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     VersionLib::VersionBuildTypeC buildType = VersionLib::initVersionBuildTypeC();
-    VersionLib::setVersionBuildTypeC(buildType, this->types.data(), this->types.size());
+    std::vector<VersionLib::VersionReleaseDataC> tmp;
+    for (size_t i = 0; i < this->types.size(); i++)
+    {
+        tmp.push_back(this->types[i].toReleaseDataC());
+    }
+    VersionLib::setVersionBuildTypeC(buildType, tmp.data(), tmp.size());
     return buildType;
-    #else
-    VersionLib::VersionBuildTypeC buildType = VersionLib::initVersionBuildTypeC();
-    VersionLib::setVersionBuildTypeC(buildType, this->type);
-    return buildType;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
+}
+
+float VersionLib::VersionBuildType::getBuildTypeWeight()
+{
+    std::vector<float> weights;
+
+    for (VersionLib::BuildRelease& release : this->types)
+    {
+        weights.push_back(release.getWeight());
+    }
+
+    return VersionLib::calcFullReleaseWeight(weights);
 }
 
 VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     this->types = other.types;
-    #else
-    this->type = other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     return *this;
 }
 
 VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(VersionLib::VersionBuildType &&other) noexcept
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     this->types = std::move(other.types);
-    #else
-    this->type = std::move(other.type);
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     return *this;
 }
 
 VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const VersionLib::BuildType& type)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     this->types.clear();
     this->types.push_back(type);
-    #else
-    this->type = type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     return *this;
 }
 
-VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const VersionLib::VersionBuildTypeC &type)
+VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const VersionLib::VersionBuildTypeC &types)
 {
     #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
+    if (types.releases != nullptr && types.size > 0)
+    {
+        this->types.clear();
+        for (size_t i = 0; i < types.size; i++)
+        {
+            this->types.push_back(types.releases[i]);
+        }
+    }
     #else
+    this->types.clear();
+    this->types.push_back(types.type)
     #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     return *this;
 }
 
-#ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
-VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const std::vector<VersionLib::BuildType> types)
+VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const VersionLib::BuildRelease type)
 {
-    if (!types.empty())
-    {
-        this->types = types;
-    }
+    this->types.clear();
+    this->types.push_back(type);
     return *this;
 }
-#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
+
+VersionLib::VersionBuildType &VersionLib::VersionBuildType::operator=(const std::vector<VersionLib::BuildRelease> types)
+{
+    this->types = types;
+    return *this;
+}
 
 bool VersionLib::VersionBuildType::operator==(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if ((this->types.size() == 0 || other.types.size() == 0) || (this->types.size() != other.types.size()))
     {
         return false;
@@ -163,9 +177,6 @@ bool VersionLib::VersionBuildType::operator==(const VersionLib::VersionBuildType
     }
 
     return true;
-    #else
-    return this->type == other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator!=(const VersionLib::VersionBuildType &other)
@@ -175,7 +186,6 @@ bool VersionLib::VersionBuildType::operator!=(const VersionLib::VersionBuildType
 
 bool VersionLib::VersionBuildType::operator<(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if ((this->types.size() == 0 || other.types.size() == 0) || (this->types.size() != other.types.size()))
     {
         return false;
@@ -190,14 +200,10 @@ bool VersionLib::VersionBuildType::operator<(const VersionLib::VersionBuildType 
     }
 
     return true;
-    #else
-    return this->type < other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator>(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if ((this->types.size() == 0 || other.types.size() == 0) || (this->types.size() != other.types.size()))
     {
         return false;
@@ -212,14 +218,10 @@ bool VersionLib::VersionBuildType::operator>(const VersionLib::VersionBuildType 
     }
 
     return true;
-    #else
-    return this->type > other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator<=(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if ((this->types.size() == 0 || other.types.size() == 0) || (this->types.size() != other.types.size()))
     {
         return false;
@@ -234,14 +236,10 @@ bool VersionLib::VersionBuildType::operator<=(const VersionLib::VersionBuildType
     }
 
     return true;
-    #else
-    return this->type <= other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator>=(const VersionLib::VersionBuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if ((this->types.size() == 0 || other.types.size() == 0) || (this->types.size() != other.types.size()))
     {
         return false;
@@ -256,22 +254,15 @@ bool VersionLib::VersionBuildType::operator>=(const VersionLib::VersionBuildType
     }
 
     return true;
-    #else
-    return this->type >= other.type;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator==(const VersionLib::BuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if (this->types.size() > 1 || this->types.empty())
     {
         return false;
     }
     return this->types[0] == other;
-    #else
-    return this->type == other;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator!=(const VersionLib::BuildType &other)
@@ -281,54 +272,38 @@ bool VersionLib::VersionBuildType::operator!=(const VersionLib::BuildType &other
 
 bool VersionLib::VersionBuildType::operator<(const VersionLib::BuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if (this->types.size() > 1 || this->types.empty())
     {
         return false;
     }
     return this->types[0] < other;
-    #else
-    return this->type < other;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator>(const VersionLib::BuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if (this->types.size() > 1 || this->types.empty())
     {
         return false;
     }
     return this->types[0] > other;
-    #else
-    return this->type > other;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator<=(const VersionLib::BuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if (this->types.size() > 1 || this->types.empty())
     {
         return false;
     }
     return this->types[0] <= other;
-    #else
-    return this->type <= other;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 bool VersionLib::VersionBuildType::operator>=(const VersionLib::BuildType &other)
 {
-    #ifdef VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
     if (this->types.size() > 1 || this->types.empty())
     {
         return false;
     }
     return this->types[0] >= other;
-    #else
-    return this->type >= other;
-    #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_SUPPORT_2_COMBINED_BUILD_TYPE
 }
 
 #endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_CLASS_BUILD_TYPE_COMPONENT
