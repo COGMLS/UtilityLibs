5974af2818603cce7ea2a93f02423aee4ff56c49
COGMLS <mlsmatheus@hotmail.com>
2025-08-14 20:26:05 -0300

    Added SemVerMap.hpp and SemVerMap.cpp

---
+++ Libraries/VersionLib/VersionLib/VersionLib/Mappers/SemVerMap.cpp
@@ -0,0 +1,24 @@
+#include "SemVerMap.hpp"
+
+#if defined(VERSION_LIB_ENABLE_EXPERIMENTAL_VERSION_TOKEN_SYSTEM) && defined(VERSION_LIB_ENABLE_EXPERIMENTAL_DICTIONARY) && defined(VERSION_LIB_ENABLE_EXPERIMENTAL_MAPPER)
+
+//
+// Semantic Versioning Mapper:
+//
+
+VersionLib::Mappers::SemVerMapper::SemVerMapper()
+{
+	this->classifier.reset(new VersionLib::Dictionaries::SemVerClassifier);
+}
+
+VersionLib::Mappers::SemVerMapper::~SemVerMapper()
+{
+	this->classifier.reset(nullptr);
+}
+
+bool VersionLib::Mappers::SemVerMapper::processTokens (std::vector<VersionLib::VersionToken>& tokens)
+{
+	return true;
+}
+
+#endif // !VERSION_LIB_ENABLE_EXPERIMENTAL_VERSION_TOKEN_SYSTEM && !VERSION_LIB_ENABLE_EXPERIMENTAL_DICTIONARY && !VERSION_LIB_ENABLE_EXPERIMENTAL_MAPPER
